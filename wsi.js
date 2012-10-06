728dcad8a2861a9e3467354ed02979d93cd1950a
kelvin <kowochen@gmail.com>
2012-10-07 03:33:11 +0800

    test ws

--- wsi.js
+++ wsi.js
@@ -1,27 +1,32 @@
 (function() {
+    Array.prototype.clean = function(deleteValue) {
+        for (var i = 0; i < this.length; i++) {
+            if (this[i] == deleteValue) {         
+                this.splice(i, 1);
+                i--;
+            }
+        }
+        return this;
+    };
+
     var logger = require('./log.js').createLogger('wsi');
 
     var WebSocketServer = require('ws').Server;
     var wss = new WebSocketServer({port: 13045});
-    wss.on('connection', function(ws) {
-        logger.debug('ws user connected');
-        ws.on('message', function(message) {
-            console.log('received: %s', message);
-        });
-        ws.send('something');
-    });
-    logger.debug('load ws');
-
-    //var io = require('socket.io').listen(13045);
-
+  
     var events = ['error', 'friends', 'tweet', 'event', 'other'];
     var buffer = [];
     var bufferSize = 0;
+    var clients = [];
 
     var refreshBuffer = function() {
         if (buffer.length) {
             buffer.forEach(function(item) {
-                //io.sockets.emit(item.event, item.data);
+                clients.forEach(function(client) {
+                    client.send(JSON.stringify(item), function(err) {
+                        logger.warn('send error: ', err);
+                    });
+                }); 
             });
             buffer = [];
         } 
@@ -39,20 +44,22 @@
     };
 
     var init = function() {
-        /*
-        io.configure(function() {
-            io.set('log level', 1);
-        });
-
-        io.sockets.on('connection', function(socket) {
-            logger.debug('user connected');
+        wss.on('connection', function(ws) {
+            logger.debug('user connected, clients: ', clients.length);
             refreshBuffer();
 
-            socket.on('disconnect', function () {
-                logger.debug('user disconnected');
+            ws.on('close', function () {
+                logger.debug('user closed');
+                clients.clean(ws);
             });
-    });
-        */
+
+            ws.on('error', function () {
+                logger.debug('user error');
+                clients.clean(ws);
+            });
+            
+            clients.push(ws);
+        });
     };
 
     var wsi = {};
@@ -60,15 +67,18 @@
          // just proxy events
         events.forEach(function(e) {
             us.on(e, function(data) {
-                /*
-                if (io.sockets.clients().length) { // if we have clients connected
+                var msg = {event: e, data: data};
+                if (clients.length) { // if we have clients connected
                     refreshBuffer();
-                    io.sockets.emit(e, data);
+                    clients.forEach(function(client) {
+                        client.send(JSON.stringify(msg), function(err) {
+                            logger.warn('send error: ', err);
+                        });
+                    }); 
                 } else {
-                    pushToBuffer({event: e, data: data});
+                    pushToBuffer(msg);
                     logger.debug('buffer len:', buffer.length);
                 }
-                */
             });
         });
     };
